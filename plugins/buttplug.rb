e5ae2cf4b001b12f1f24eef072ba092bc86b1c15
Nora Maguire <eva@rigel.moe>
2018-02-14 03:11:57 -0600

    Ok Perverted fore into metaprogramming done
    as in ... yeah devices now can accept controllers that vibrate ... neat.

--- plugins/buttplug.rb
+++ plugins/buttplug.rb
@@ -2,6 +2,7 @@ require 'faye/websocket'
 require 'eventmachine'
 require 'json'
 module Buttplug
+
   class Client
     def initialize(serverLocation)
       @location=serverLocation
@@ -37,7 +38,7 @@ module Buttplug
           ws = nil
         end
         EM.add_periodic_timer(0.5){
-          ws.send "[{\"Ping\": {\"Id\": #{rand(2..4294967295)}}}]"
+          ws.send "[{\"Ping\": {\"Id\": #{generateID()}}}]"
           eventQueue.pop{|msg|
             ws.send msg[1]
             messageWatch[msg[0]]=msg
@@ -47,6 +48,66 @@ module Buttplug
       }}
       @eventMachine.run
     end
+    def startScanning()
+      id=generateID()
+      @eventQueue.push([id,"[{\"StartScanning\":{\"Id\":#{id}}}]"])
+    end
+    def stopScanning()
+      id=generateID()
+      @eventQueue.push([id,"[{\"StopScanning\":{\"Id\":#{id}}}]"])
+    end
+    def listDevices()
+      id=generateID()
+      deviceRequest=[id,"[{\"RequestDeviceList\": {\"Id\":#{id}}}]"]
+      @eventQueue.push(deviceRequest)
+      while(deviceRequest.length<3) do
+        sleep 0.01#Just so we arn't occupying all the time on the system while we are waiting for our device list to come back.
+      end
+      return deviceRequest[2]["DeviceList"]["Devices"]
+    end
+    def sendMessage(message)
+      @eventQueue.push(message)
+      while(message.length<3) do
+        sleep 0.01
+      end
+    end
+    def generateID()
+      return rand(2..4294967295)
+    end
+  end
+  class Device
+    def initialize(client, deviceInfo)
+      #Ok we are gonna expect our deviceInfo to be a Hash so we can do some ... fun things ...
+      #{"DeviceName"=>"XBox Compatible Gamepad (XInput)", "DeviceIndex"=>1, "DeviceMessages"=>{"SingleMotorVibrateCmd"=>{}, "VibrateCmd"=>{"FeatureCount"=>2}
+      @deviceName=deviceInfo["DeviceName"]
+      @deviceIndex=deviceInfo["DeviceIndex"]
+      @client=client
+      #Ok so we are starting our weird metaProgramming BS here
+      if(deviceInfo["DeviceMessages"].keys.include? "VibrateCmd")
+        @vibeMotors=deviceInfo["DeviceMessages"]["VibrateCmd"]["FeatureCount"]
+        define_singleton_method(:vibrate){|speeds|
+          #And now the real fun, we are gonna craft our message!
+          id=client.generateID()
+          cmd=[{"VibrateCmd"=>{"Id"=>id,"DeviceIndex"=>@deviceIndex,"Speeds"=>[]}}]
+          #Ok we arn't gonna really care about how many speeds we are fed in here, we are gonna make sure that our total array isn't empty.
+          (0..@vibeMotors-1).each{|i|
+            if speeds[i].nil?
+              speeds[i]=0
+            end 
+            cmd[0]["VibrateCmd"]["Speeds"]<<{"Index"=>i,"Speed"=>speeds[i]}
+          }
+          client.sendMessage([id,cmd.to_json])
+        }
+        define_singleton_method(:vibrateAll){|speed|
+          speeds=[]
+          (0..@vibeMotors-1).each{|i|
+            speeds<<speed
+          }
+          vibrate(speeds)
+        }
+      end
+    end
+
   end
 end
 
@@ -56,11 +117,22 @@ if defined? MEGUMIN
         $bot.send_file(event.channel.id,File.new("#{APPDIR}/assets/UOCEbLn.png"))
     end
 elsif __FILE__==$0
-  request=[379,'[{"RequestDeviceList": {"Id":379}}]']
-  puts "Original Request:#{request}"
   client=Buttplug::Client.new("wss://192.168.1.40:6969/buttplug")
-  sleep 0.2
-  client.instance_variable_get(:@eventQueue).push(request)
-  sleep 2
-  puts "Finished Request:#{request}"
+  sleep 0.1
+  client.startScanning();
+  sleep 1
+  devices=client.listDevices()
+  client.stopScanning()
+  controller=Buttplug::Device.new(client,devices[0])
+ # if(request.length==2)#Ok, So something has gone wrong if our request length is 2. it should have returned something.
+    #client.instance_variable_get(:@eventMachine).join
+  #end
+  #ok we are gonna make a few assumptions here ... primarily that you are using an xbox controller
+  if(controller.methods.include? :vibrateAll)
+    (0..20).each{|i|
+      controller.vibrateAll(rand(0.0..1.0))
+      sleep(rand(0.5..2))
+    }
+    controller.vibrateAll(0.0)
+  end
 end
